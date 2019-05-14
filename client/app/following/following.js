4b50033877b6e25d9288a99e7d7338ad9ffa9d7f
Chom007 <yawana123@hotmail.com>
2019-05-14 18:51:43 +0700

    Homwork 5 is finished

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -3,15 +3,35 @@
 angular.module('followingList', ['ngRoute'])
   .component('followingList', {
     templateUrl: 'following/following.html',
-    controller: ['$http', '$rootScope', function TweetListController($http, $rootScope) {
+    controller: ['$http', '$rootScope', function TweetListController($http, $rootScope)
+    {
       var self = this;
 
-      const requestOptions = {
+      const requestOptions =
+      {
           headers: { 'X-session': $rootScope.x_session }
       };
 
-      $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
+      $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response)
+      {
         self.followings = response.data;
       });
+      self.sendFollow = function sendFollow(followingname)
+      {
+
+          const data = "followingname=" + encodeURIComponent(followingname);
+          $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions).then(function (response) {
+              console.log(response);
+          });
+      }
+      self.sendUnFollow = function sendUnFollow(followingname)
+      {
+          $http.defaults.headers.delete = { 'X-session': $rootScope.x_session, "Content-Type": "application/json;charset=utf-8" };
+          const data = "followingname=" + encodeURIComponent(followingname);
+          console.log(data);
+          $http.delete('http://localhost:8080/twitterapi/following/?' + data).then(function (response) {
+              console.log(response);
+          });
+      }
     }]
 });
